9ee29c856e88e755d4fa46ac821cd09393329556
Olatunji Ajibode <ola.ajibode@gmail.com>
2019-12-09 17:36:11 +0000

    rename main class

---
+++ src/main/java/com/ohprice/analytics/Analytics.java
@@ -0,0 +1,13 @@
+package com.ohprice.analytics;
+
+/**
+ * Analytics App
+ *
+ */
+public class Analytics
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
